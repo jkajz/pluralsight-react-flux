bd89ee8a0808bf3df646489161a7683486ee531a
Jani <jani.kajzovar@gmail.com>
2016-10-12 19:39:28 +0200

    First basic components and custom routing

--- src/main.js
+++ src/main.js
@@ -1,4 +1,33 @@
-$ = JQuery = require('jquery');
-var App = console.log('Hello world from Browserify');
 
-module.exports = App;
+$ = JQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var Authors = require('./components/authors/authorPage');
+var About = require('./components/about/aboutPage');
+var Header = require('./components/common/header');
+
+var App = React.createClass({
+  render: function() {
+    var Child;
+
+    switch(this.props.route) {
+      case 'about': Child = About; break;
+      case 'authors': Child = Authors; break;
+      default: Child = Home;
+    }
+
+    return (
+      <div>
+        <Header />
+        <Child />
+      </div>
+    );
+  }
+});
+
+function render() {
+  var route = window.location.hash.substr(1);
+  React.render(<App route={route} />, document.getElementById('app'));
+}
+window.addEventListener('hashchange', render);
+//React.render(<Home />, document.getElementById('app'));
